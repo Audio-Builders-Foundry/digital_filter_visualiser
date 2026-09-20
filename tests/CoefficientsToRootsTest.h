dbd8c886d86dd7d000ed4d9a2d56594555857775
Melissas Paschalis <melissaspaschalis@gmail.com>
2026-09-21 01:54:51 +0300

    implemennt test generator for root order tests

--- tests/CoefficientsToRootsTest.h
+++ tests/CoefficientsToRootsTest.h
@@ -6,6 +6,7 @@
 #include "../src/PluginProcessor.h"
 #include "../src/CoefficientsToRoots.h"
 #include "../src/RootsToCoefficients.h"
+#include "generator/root_order_tests.h"
 
 
 class CoefficientsToRootsTest : public juce::UnitTest
@@ -14,95 +15,6 @@ public:
     CoefficientsToRootsTest() : UnitTest("CoefficientsToRootsTest", "Math")
 	{
 	}
-
-#define COEFFICIENTS_TO_ROOTS_TEST_XLIST\
-  X("1:1 real pole order 1", { {-1, -0.5, 0} } )\
-  X("2:1 real pole order 2", { {-2, -0.5, 0} } )\
-  X("2:2 real poles order 1", { {-1, -0.5, 0}, {-1, 0.3, 0} } )\
-  X("2:1 complex pole order 1 a", { {-1, 0.3, 0.4} } )\
-  X("2:1 complex pole order 1 b", { {-1, 0.4, 0.5} } )\
-  X("2:1 complex pole order 1 c", { {-1, 0.6, 0.7} } )\
-  X("3:1 real pole order 2 + order 1", { {-2, -0.01, 0}, {-1, 0.3, 0} } )\
-  X("3:1 real pole order 3 a", { {-3, -0.5, 0} } )\
-  X("3:1 real pole order 3 b", { {-3, -0.9, 0} } )\
-  X("3:3 real poles order 1", { {-1, -0.5, 0}, {-1, 0.3, 0}, {-1, 0.2, 0} } )\
-  X("3:1 complex order 1 + 1 real order 1 a", { {-1, 0.3, 0.4}, {-1, -0.5, 0} } )\
-  X("3:1 complex order 1 + 1 real order 1 b", { {-1, 0.3, 0.4}, {-1, 0.5, 0} } )\
-  X("3:1 complex order 1 + 1 real order 1 c", { {-1, 0.3, 0.4}, {-1, -0.8, 0} } )\
-  X("4:1 real order 4", { {-4, -0.33, 0} })\
-  X("4:2 real order 1 + 1 real order 2", { {-1,-0.5,0}, {-1,0.3,0}, {-2,0.2,0} })\
-  X("4:4 real poles order 1", { {-1,-0.5,0}, {-1,0.3,0}, {-1,0.2,0}, {-1,0.1,0} } )\
-  X("4:2 real poles order 2", { {-2,-0.5,0}, {-2,0.3,0} } )\
-  X("4:2 complex poles order 1 a", { {-1,0.3,0.4}, {-1,-0.1,0.3} } )\
-  X("4:2 complex poles order 1 b", { {-1,0.3,0.4}, {-1,0.1,-0.3} } )\
-  X("4:1 real order 3 + 1 real order 1", { {-3,-0.5,0}, {-1,0.3,0} } )\
-  X("5:1 real pole order 5", { {-5,-0.5,0} } )\
-  X("5:1 real order 4 + 1 real order 1 a", { {-4,-0.5,0}, {-1,0.1,0} } )\
-  X("5:1 real order 4 + 1 real order 1 b", { {-4,-0.6,0}, {-1,0.2,0} } )\
-  X("5:1 real order 4 + 1 real order 1 c", { {-4,-0.7,0}, {-1,0.3,0} } )\
-  X("5:1 real order 4 + 1 real order 1 d", { {-4,-0.8,0}, {-1,0.7,0} } )\
-  X("5:1 real order 3 + 1 real order 2 a", { {-3,-0.1,0}, {-2,0.3,0} } )\
-  X("5:1 real order 3 + 1 real order 2 b", { {-3,-0.9,0}, {-2,0.7,0} } )\
-  X("5:1 real order 3 + 1 complex order 1 a", { {-3,-0.5,0}, {-1,0.1,0.4} } )\
-  X("5:1 real order 3 + 1 complex order 1 b", { {-3,-0.1,0}, {-1,0.2,0.3} } )\
-  X("5:1 real order 1 + 1 complex order 2 a", { {-1,-0.5,0}, {-2,0.3,0.1} } )\
-  X("5:1 real order 1 + 1 complex order 2 b", { {-1,-0.5,0}, {-2,0.4,0.44} } )\
-  X("5:2 real order 2 + 1 real order 1", { {-2,-0.5,0}, {-2,0.3,0}, {-1,0.2,0} } )\
-  X("5:1 real order 2 + 3 real order 1", { {-2,-0.5,0}, {-1,0.3,0}, {-1,0.2,0}, {-1,0.1,0} } )\
-  X("5:5 real poles order 1 a", { {-1,-0.5,0}, {-1,0.3,0}, {-1,0.2,0}, {-1,0.1,0}, {-1,-0.3,0} } )\
-  X("5:5 real poles order 1 b", { {-1,-0.1,0}, {-1,0.2,0}, {-1,0.3,0}, {-1,0.4,0}, {-1,-0.5,0} } )\
-  X("5:1 real order 1 + 1 complex + 2 real order 1 a", { {-1,-0.5,0}, {-1,0.3,0.4}, {-1,0.2,0}, {-1,0.1,0} } )\
-  X("5:1 real order 1 + 1 complex + 2 real order 1 b", { {-1,-0.2,0}, {-1,0.1,0.4}, {-1,0.4,0}, {-1,0.8,0} } )\
-  X("5:1 real order 1 + 2 complex order 1 a", { {-1,-0.1,0}, {-1,0.3,0.4}, {-1,-0.1,0.3} } )\
-  X("5:1 real order 1 + 2 complex order 1 b", { {-1,-0.9,0}, {-1,0.13,0.4}, {-1,-0.13,0.53} } )\
-  X("6:1 real pole order 6 a", { {-6,-0.01,0} } )\
-  X("6:1 real pole order 6 b", { {-6,-0.99,0} } )\
-  X("6:1 real order 5 + 1 real order 1", { {-5,-0.5,0}, {-1,0.3,0} } )\
-  X("6:1 real order 4 + 1 complex order 1 a", { {-4,-0.05,0}, {-1,0.13,0.14} } )\
-  X("6:1 real order 4 + 1 complex order 1 b", { {-4,-0.95,0}, {-1,0.31,0.41} } )\
-  X("6:1 real order 4 + 2 real order 1 a", { {-4,-0.1,0}, {-1,0.35,0}, {-1,0.12,0} } )\
-  X("6:1 real order 4 + 2 real order 1 b", { {-4,-0.6,0}, {-1,0.63,0}, {-1,0.21,0} } )\
-  X("6:1 real order 3 + 3 real order 1", { {-3,-0.5,0}, {-1,0.3,0}, {-1,0.2,0}, {-1,0.1,0} } )\
-  X("6:1 real order 3 + 1 complex + 1 real order 1", { {-3,-0.5,0}, {-1,0.3,0.4}, {-1,0.2,0} } )\
-  X("6:1 complex pole order 3", { {-3,0.99,0.4} } )\
-  X("6:1 complex pole order 3 b", { {-3,0.1,0.4} } )\
-  X("6:3 real poles order 2", { {-2,-0.5,0}, {-2,0.3,0}, {-2,0.2,0} } )\
-  X("6:2 real order 2 + 1 complex order 1", { {-2,-0.5,0}, {-2,0.3,0}, {-1,0.2,0.3} } )\
-  X("6:1 real order 2 + 2 complex order 1", { {-2,-0.5,0}, {-1,0.3,0.4}, {-1,-0.1,0.3} } )\
-  X("6:3 complex poles order 1", { {-1,0.3,0.4}, {-1,-0.1,0.3}, {-1,0.1,0.2} } )\
-  X("7:1 real pole order 7 a", { {-7,-0.9,0} } )\
-  X("7:1 real pole order 7 b", { {-7,-0.1,0} } )\
-  X("7:1 real order 6 + 1 real order 1", { {-6,-0.5,0}, {-1,0.3,0} } )\
-  X("7:1 real order 5 + 1 complex order 1 a", { {-5,-0.5,0}, {-1,0.9,0.1} } )\
-  X("7:1 real order 5 + 1 complex order 1 b", { {-5,-0.5,0}, {-1,0.3,0.4} } )\
-  X("7:1 real order 4 + 1 complex + 1 real order 1", { {-4,-0.5,0}, {-1,0.3,0.4}, {-1,0.2,0} } )\
-  X("7:1 real order 4 + 3 real order 1", { {-4,-0.5,0}, {-1,0.3,0}, {-1,0.2,0}, {-1,0.1,0} } )\
-  X("7:1 real order 3 + 1 complex order 2", { {-3,-0.5,0}, {-2,0.3,0.4} } )\
-  X("7:1 complex order 3 + 1 real order 1", { {-3,0.3,0.4}, {-1,-0.5,0} } )\
-  X("7:3 complex + 1 real order 1", { {-1,0.3,0.4}, {-1,-0.1,0.3}, {-1,0.1,0.2}, {-1,-0.5,0} } )\
-  X("7:1 complex + 5 real order 1", { {-1,0.3,0.4}, {-1,-0.5,0}, {-1,0.2,0}, {-1,0.1,0}, {-1,-0.3,0}, {-1,-0.2,0} } )\
-  X("7:7 real poles order 1", { {-1,-0.5,0},{-1,0.3,0},{-1,0.2,0},{-1,0.1,0},{-1,-0.3,0},{-1,-0.2,0},{-1,0.4,0} } )\
-  X("8:1 real pole order 8", { {-8,-0.5,0} } )\
-  X("8:1 real order 7 + 1 real order 1", { {-7,-0.5,0}, {-1,0.3,0} } )\
-  X("8:1 real order 6 + 1 complex order 1", { {-6,-0.5,0}, {-1,0.3,0.4} } )\
-  X("8:8 real poles order 1", { {-1,-0.5,0},{-1,0.3,0},{-1,0.2,0},{-1,0.1,0},{-1,-0.3,0},{-1,-0.2,0},{-1,0.4,0},{-1,-0.4,0} } )\
-  X("8:1 complex + 6 real order 1", { {-1,0.3,0.4},{-1,-0.5,0},{-1,0.2,0},{-1,0.1,0},{-1,-0.3,0},{-1,-0.2,0},{-1,0.4,0} } )\
-  X("8:2 complex + 4 real order 1", { {-1,0.3,0.4},{-1,-0.1,0.3},{-1,-0.5,0},{-1,0.2,0},{-1,0.1,0},{-1,-0.3,0} } )\
-  X("8:4 complex poles order 1", { {-1,0.3,0.4},{-1,-0.1,0.3},{-1,0.1,0.2},{-1,-0.2,0.1} } )\
-  X("9:1 real pole order 9", { {-9,-0.5,0} } )\
-  X("9 real poles order 1", { {-1,-0.5,0},{-1,0.3,0},{-1,0.2,0},{-1,0.1,0},{-1,-0.3,0},{-1,-0.2,0},{-1,0.4,0},{-1,-0.4,0},{-1,0.45,0} } )\
-  X("9:1 complex + 7 real order 1", { {-1,0.3,0.4},{-1,-0.5,0},{-1,0.2,0},{-1,0.1,0},{-1,-0.3,0},{-1,-0.2,0},{-1,0.4,0},{-1,-0.4,0} } )\
-  X("9:1 real order 5 + 1 real order 4", { {-5,-0.5,0}, {-4,0.3,0} } )\
-  X("9:1 real order 5 + 1 complex order 2", { {-5,-0.5,0}, {-2,0.3,0.4} } )\
-  X("10:1 real pole order 10", { {-10,-0.5,0} } )\
-  X("10:2 real poles order 5", { {-5,-0.5,0}, {-5,0.3,0} } )\
-  X("10:1 real order 5 + order 4 + order 1", { {-5,-0.5,0}, {-4,0.3,0}, {-1,0.2,0} } )\
-  X("10:1 real order 4 + order 3 + order 2 + order 1", { {-4,-0.5,0},{-3,0.3,0},{-2,0.2,0},{-1,0.1,0} } )\
-  X("10:1 real order 4 + order 3 + complex + order 1", { {-4,-0.5,0},{-3,0.3,0},{-1,0.2,0.3},{-1,0.1,0} } )\
-  X("14:1 real pole order 14", { {-14,-0.5,0} } )\
-  X("20:1 real pole order 20", { {-20,-0.5,0} } )\
-  X("28:1 real pole order 28", { {-28,-0.5,0} } )\
-  X("32:1 real pole order 32", { {-32,-0.5,0} } )\
 
     void runTest() override
     {
--- tests/generator/generate.cpp
+++ tests/generator/generate.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 
 #define DISTANCE_TEST_HEADER_FILENAME "root_distance_tests.h"
+#define ROOTORDER_TEST_HEADER_FILENAME "root_order_tests.h"
 
 float get_random_float()
 {
@@ -11,7 +12,7 @@ float get_random_float()
     return dis(e);
 }
 
-int get_num_tests(int order)
+int get_num_tests(int order, int ymin, int ymax, int x0, float k)
 {
     /*
     // Reverse of the Sigmoid function shifted horizontaly:
@@ -20,11 +21,12 @@ int get_num_tests(int order)
     //      [ymin,ymax] is the range of examples,
     //      x0 is the function has declined by 50%
     //      k defines the steepness of the declining curve
+    // Example:
     // So for ymax=10, ymin=3, x0=20 and k=0.3, the function becomes:
     // f(x) = 3 + 7 / (1 + e^0.3(x-20))
     // ref:check f\left(x\right)=3\ +\ \frac{7}{\left(1+e^{0.3\left(x-20\right)}\right)} in https://www.desmos.com/calculator
     */
-    return static_cast<int>(std::round(3 + 7 / (1 + std::exp( 0.3 * (order-20)))));
+    return static_cast<int>(std::round( ymin + (ymax-ymin) / (1 + std::exp( k * static_cast<float>(order-x0)))));
 }
 
 void generate_root_distance_tests(int argc, char* argv[])
@@ -35,8 +37,6 @@ void generate_root_distance_tests(int argc, char* argv[])
     }
 
     std::ofstream outStream(filename);
-
-    srand (static_cast <unsigned> (time(0)));
 
     std::string rootTypes[2] = {"pole", "zero"};
     std::string dts[2] = {"real","complex"};
@@ -52,8 +52,8 @@ void generate_root_distance_tests(int argc, char* argv[])
         for (int order=1;order<=32;++order)
         {
             int orderCtr=0;
-            int num_tests = get_num_tests(order);
-            std::cout<<"Order "<<order<<" generates "<<num_tests<<" tests."<<std::endl;
+            int num_tests = get_num_tests(order, 3, 10, 20, .3);
+
             for (int sample=0; sample<num_tests; sample++ )
             {
 
@@ -82,9 +82,136 @@ void generate_root_distance_tests(int argc, char* argv[])
     outStream.close();
 }
 
+void partition_rootOrder(int n, std::vector<std::vector<int> >& partitions)
+{
+    /*
+    Modified the printAllUniqueParts function drawn from www.sanfoundry.com 
+    Reference : https://www.sanfoundry.com/cpp-program-generate-all-unique-partitions-integer/ 
+    */
+
+    int p[n]; // An array to store a partition
+    int k = 0;  // Index of last element in a partition
+    p[k] = n;  // Initialize first partition as number itself
+    int i = 0; // Index of current partition
+
+    // This loop first prints the current partition then generates the next partition. 
+    // The loop stops when the current partition has all 1s
+    while (true)
+    {
+        // store new partition
+        partitions.push_back(std::vector<int>(p, p + k+1));
+ 
+        // Find the rightmost non-one value in p[]. Also, update the rem_val
+        // So that we know how much value can be accommodated
+ 
+        int rem_val = 0;
+        while (k >= 0 && p[k] == 1)
+        {
+            rem_val += p[k];
+            k--;
+        }
+ 
+        // if k < 0, all the values are 1 so there are no more partitions
+ 
+        if (k < 0)  
+            return;
+ 
+        // Decrease the p[k] found above and adjust the rem_val
+        p[k]--;
+        rem_val++;
+ 
+        // If rem_val is more, then the sorted order is violeted.  
+        // Divide rem_val in differnt values of size p[k] 
+        // Copy these values at different positions after p[k]
+        while (rem_val > p[k])
+        {
+            p[k+1] = p[k];
+            rem_val = rem_val - p[k];
+            k++;
+        }
+
+        // Copy rem_val to next position and increment position
+        p[k+1] = rem_val;
+        k++;
+    }
+}
+
+void generate_root_order_tests(int argc, char* argv[])
+{
+    std::string filename(ROOTORDER_TEST_HEADER_FILENAME);
+    if (argc==2){
+        filename = std::string(argv[1]) + "/" + filename;
+    }
+
+    std::ofstream outStream(filename);
+    outStream << "#define COEFFICIENTS_TO_ROOTS_TEST_XLIST\\"<<'\n';
+
+    std::string dimtypes[2] = {"real","complex"};
+
+    // for each order
+    for (int order=1;order<=32;++order)
+    {
+        int orderCtr=0;
+        int num_tests = get_num_tests(order, 3, 20, 17, 0.7);
+        std::vector<std::vector<int> > partitions;
+        partition_rootOrder(order, partitions);
+        float ratio = static_cast<float>(partitions.size()) / num_tests;
+
+        // for each test sample
+        for (int sample=0; sample<num_tests; sample++ )
+        {
+            int index = static_cast<int>(sample * ratio);
+            
+            // std::cout<<"order "<<order<<" sample "<<sample<<"/"<<num_tests<<" index "<<index<<"/"<<partitions.size()<<std::endl;
+            size_t numRoots = partitions[index].size();
+
+            std::string testLine = "X(\"" + std::to_string(order) + "("+std::to_string(++orderCtr)+"):Pole order config (";
+
+            std::string values = "{";
+            
+            // for each sub root
+            for (size_t i=0; i< numRoots; i++)
+            {    
+                int subRootOrder = partitions[index][i];
+                
+                std::string dt = dimtypes[(order+i)%2];
+                
+                // edit config text
+                testLine+= (std::to_string(subRootOrder)+" "+dt);
+
+                // edit root value text
+                float x,y;
+                if (dt.compare("complex")){
+                    y = 0.0f;
+                    x= get_random_float();
+                }
+                else{
+                    y = get_random_float();
+                    x= get_random_float();
+                }
+                values += "{" + std::to_string(-subRootOrder) + "," + (std::to_string(x) + "," + std::to_string(y) + "}");
+
+                if (i!=numRoots-1) 
+                {
+                    testLine+=",";
+                    values+=",";
+                }
+            }
+
+            testLine += ")\"," + values + "} )\\";
+            outStream << '\t' << testLine << '\n';
+        }
+    }
+    outStream.close();
+}
 
 int main(int argc, char* argv[]){
 
+    // seed for generating deterministic random values
+    srand (static_cast <unsigned> (time(0)));
+
     generate_root_distance_tests(argc, argv);
+
+    generate_root_order_tests(argc, argv);
 
 }
